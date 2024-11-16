3444623e93a735c7875ed00dc09b0f971ad6ad7c
Laurent Maxhuni <laurentmaxhuni1234@gmail.com>
2024-11-16 16:53:54 +0100

    Created Input.jsx Component to use in future pages.

---
+++ src/components/Input.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Input(props) {
+  return (
+    <>
+      <div className={props.className}>
+        <span className={props.spanClassName} id="basic-addon1">
+          {props.icon}
+        </span>
+        {props.inputClassName && (
+          <input
+            type={props.type}
+            value={props.value}
+            className={props.inputClassName}
+            onChange={props.onChange}
+            placeholder={props.placeholder}
+            required
+            checked={props.checked}
+            aria-label={props.ariaLabel}
+            aria-describedby="basic-addon1"
+          />
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Input;
