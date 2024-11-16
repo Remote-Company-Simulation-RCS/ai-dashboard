cee2952cd77ade5f38f19e971a627774f9343e5b
Laurent Maxhuni <laurentmaxhuni1234@gmail.com>
2024-11-16 18:45:25 +0100

    Finished dark mode for sign up

--- src/components/SignUpCards.jsx
+++ src/components/SignUpCards.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function SignUpCards(props) {
   return (
-    <div className="col-12 col-lg-4 col-xl-4 card d-flex justify-content-center" style={{ width: props.width, backgroundImage: `url(${props.bgImage})` }} >
-      <button className="text-start border-0 bg-transparent">
+    <div className="col-12 col-lg-4 col-xl-4 card d-flex justify-content-center" style={{ width: props.width, backgroundImage: `url(${props.bgImage})`}} >
+      <button className="text-start border-0 bg-transparent" style={{ color: `var(--${props.color}) !important` }}>
         <div>{props.icon}</div>
-        <p className="text-white-50 mb-0 mt-3">
+        <p className="mb-0 mt-3">
           {props.action}
           <br />
-          <span className="text-white">{props.app}</span>
+          <span>{props.app}</span>
         </p>
       </button>
     </div>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,21 +1,102 @@
-import React, { useState, useEffect } from "react";
-import SignUpDark from "../components/Auth/SignUpDark.jsx";
-import SignUpLight from "../components/Auth/SignUpLight.jsx";
+import React, { useState } from "react";
+import "../styles/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Logo from "../assets/icons/logo.svg";
+import { AiOutlineUser } from "react-icons/ai";
+import { LuLuggage } from "react-icons/lu";
+import { FaArrowRightLong, FaApple, FaCirclePlay } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+import Button from "../components/Button.jsx";
+import SignUpCards from "../components/SignUpCards.jsx";
+import bgImage from "../assets/images/bg-icon.jpg";
+import Input from "../components/Input.jsx";
 
-function SignUpComponent() {
-  const [isDarkMode, setIsDarkMode] = useState(
-    window.matchMedia("(prefers-color-scheme: dark)").matches
+function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [company, setCompany] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleCompanyChange = (e) => {
+    setCompany(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Company:", company);
+  };
+
+  return (
+    <div className="sign-up dark-signup d-flex flex-column align-items-center text-center">
+      <img src={Logo} className="img-fluid py-5" alt="icon image" />
+      <h2 className="title-signup text-center mb-5 ">Create account</h2>
+      <p className="desc-signup lead">
+        Start your 30-day free trial. Cancel anytime.
+      </p>
+      <form className="d-flex flex-wrap flex-md-nowrap mx-3 flex-row gap-3 align-items-center mb-3">
+        <Input
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="Email"
+          ariaLabel="email"
+          icon={<AiOutlineUser className="text-secondary" />}
+          className="input-group border-3"
+          spanClassName="input-group-text border-secondary bg-transparent border-end-0"
+          inputClassName="form-control bg-transparent border-start-0 p-3 border-secondary"
+        />
+        <Input
+          type="text"
+          value={company}
+          onChange={handleCompanyChange}
+          placeholder="Company"
+          ariaLabel="company"
+          icon={<LuLuggage className="text-secondary" />}
+          className="input-group border-3"
+          spanClassName="input-group-text border-secondary bg-transparent border-end-0"
+          inputClassName="form-control bg-transparent border-start-0 p-3 border-secondary"
+        />
+        <Button
+          type="submit"
+          className="btn btn-primary rounded-circle next-btn d-flex justify-content-center mx-auto"
+          onClick={handleSubmit}
+          children={<FaArrowRightLong className="text-white fs-4" />}
+        />
+      </form>
+      <p className="text-center text-secondary">
+        Have an account?
+        <a href="#" className="signin-herf text-decoration-none">
+          &nbsp; Try to sign in
+        </a>
+      </p>
+      <div className="row d-flex justify-content-evenly px-2 px-md-5 flex-column align-items-center flex-md-row gap-3">
+        <SignUpCards
+          icon={<FcGoogle className="display-6" />}
+          action="Sign Up"
+          app="with Google"
+          width={150}
+          bgImage=""
+        />
+        <SignUpCards
+          icon={<FaApple className="display-6" />}
+          action="Sign Up"
+          app="with Apple"
+          width={150}
+          bgImage=""
+        />
+        <SignUpCards
+          icon={<FaCirclePlay className="display-6" />}
+          action="Watch how Quickit"
+          app="can solve your problems"
+          width={250}
+          bgImage={bgImage}
+          color="white"
+        />
+      </div>
+    </div>
   );
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    const handleChange = (e) => setIsDarkMode(e.matches);
-
-    mediaQuery.addEventListener("change", handleChange);
-    return () => mediaQuery.removeEventListener("change", handleChange);
-  }, []);
-
-  return isDarkMode ? <SignUpDark /> : <SignUpLight />;
 }
 
-export default SignUpComponent;
+export default SignUpPage;
