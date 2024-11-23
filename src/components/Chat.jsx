a7929847b3681a777f98bb5796b2d532dd9be469
Laurent Maxhuni <laurentmaxhuni1234@gmail.com>
2024-11-23 19:11:22 +0100

    Added Main Funcitonality to Assistant Page

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState} from "react";
 import Input from "../components/Input.jsx";
 import Button from "../components/Button.jsx";
 import { BsArrowReturnRight } from "react-icons/bs";
 import "../styles/main.css";
 import "../styles/components/chat.css";
+import { main } from "../components/Groq.jsx";
 
 export default function Chat() {
+  const [userMessage, setUserMessage] = useState("");
+
+
+  function callMain() {
+    main(userMessage);
+    setUserMessage("");
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      callMain();
+    }
+  }
+
+  function handleButtonClick() {
+    callMain();
+  }
+
   return (
-    <div className="chat m-3 mt-4 rounded-5 p-4 d-flex">
+    <div className="chat m-3 mt-4 rounded-5 p-4 d-flex flex-column">
+      <div className="chat-messages" id="chat-messages">
+    
+      </div>
       <Input
         type="text"
         placeholder="Start typing . . ."
         className="input-group w-100 d-flex flex-row-reverse align-self-end"
         spanClassName="chat-input input-group-text border-secondary bg-transparent border-start-0 "
         inputClassName="chat-input form-control bg-transparent border-end-0 p-3 px-3 px-md-5 border-secondary"
+        onChange={(e) => setUserMessage(e.target.value)}
+        onKeyDown={handleKeyPress}
+        value={userMessage}
         icon={
           <Button
             type="submit"
             className="btn btn-buttons text-start rounded-pill"
+            onClick={handleButtonClick}
             children={
               <div>
                 <BsArrowReturnRight size={18} />
---
+++ src/components/Groq.jsx
@@ -0,0 +1,68 @@
+import Groq from "groq-sdk";
+
+const groq = new Groq({
+  apiKey: process.env.REACT_APP_GROQ_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+let messageHistory = [];
+const regexBold = /\*\*([^*]+)\*\*/g;
+const regexBullet = /\*([^*]+)\*/g;
+const regexPreformatted = /\`\`\`([^\`]+)\`\`\`/g;
+const regexInlineCode = /\`([^`]+)\`/g;
+
+export async function main(content) {
+  messageHistory.push({
+    role: "system",
+    content:
+      "Try to be as helpful as possible, reply with the best of your abilites, and don't reply with unnecessarily long answers. Always put code behin ``` ``` even if it's for a demo!",
+  });
+  let chatMessages = document.getElementById("chat-messages");
+
+  chatMessages.innerHTML += `<div class="user-message"><div class="user-content">${content
+    .replace(regexPreformatted, (match, p1) => {
+      return `<pre class="code-block"><code>${escapeHTML(p1)}</code></pre>`;
+    })
+    .replace(regexBold, (match, p1) => `<strong>${p1}</strong>`)
+    .replace(regexBullet, (match, p1) => `<ul><li>${p1}</li></ul>`)
+    .replace(
+      regexInlineCode,
+      (match, p1) => `<code>${escapeHTML(p1)}</code>`
+    )}</div></div>`;
+
+  messageHistory.push({
+    role: "user",
+    content: content,
+  });
+
+  const chatCompletion = await getGroqChatCompletion(messageHistory);
+  function escapeHTML(str) {
+    return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  }
+
+  chatMessages.innerHTML += `<div class="assistant-message"><div class="assistant-content">
+  ${(chatCompletion.choices[0]?.message?.content || "")
+    .replace(regexPreformatted, (match, p1) => {
+      return `<pre class="code-block"><code>${escapeHTML(p1)}</code></pre>`;
+    })
+    .replace(regexBold, (match, p1) => `<strong>${p1}</strong>`)
+    .replace(regexBullet, (match, p1) => `<ul><li>${p1}</li></ul>`)
+    .replace(
+      regexInlineCode,
+      (match, p1) => `<code>${escapeHTML(p1)}</code>`
+    )}</div></div>`;
+
+  messageHistory.push({
+    role: "assistant",
+    content: chatCompletion.choices[0]?.message?.content || "",
+  });
+}
+
+export async function getGroqChatCompletion(messages) {
+  return groq.chat.completions.create({
+    messages: messages,
+    model: "llama3-8b-8192",
+    temperature: 0.69,
+    top_p: 1,
+  });
+}
