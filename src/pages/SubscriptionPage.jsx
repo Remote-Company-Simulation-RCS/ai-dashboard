f57f87eb5b3184d59b930520b452d1e8cd3677ab
ezdrit <bunjakuezdrit@gmail.com>
2024-11-23 22:14:05 +0100

    add first cards

--- src/pages/SubscriptionPage.jsx
+++ src/pages/SubscriptionPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import '../styles/pages/subscription.css';
-
+import React from "react";
+import Navbar from "../components/Navbar.jsx";
+import SubTable from "../components/SubTable.jsx";
+import "../styles/pages/subscription.css";
 
 export default function SubscriptionPage() {
-    return ( <>
-    
-    </> );
+  return (
+    <div className="subscription-page-container">
+      <Navbar active="subscription" />
+      <SubTable />
+    </div>
+  );
 }
