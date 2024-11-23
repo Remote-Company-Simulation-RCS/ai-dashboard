f2f81de9b313b58c8ca1a59d58137f7298655929
ezdrit <bunjakuezdrit@gmail.com>
2024-11-23 15:51:23 +0100

    start subscription

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AssistantPage from "./pages/AssistantPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import SignInPage from "./pages/SignInPage.jsx";
+import SubscriptionPage from "./pages/SubscriptionPage.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<SignInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/assistant" element={<AssistantPage />} />
+          <Route path="/subscription" element={<SubscriptionPage />} />
         </Routes>
       </div>
     </Router>
