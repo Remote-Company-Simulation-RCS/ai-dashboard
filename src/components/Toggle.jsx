0fb65761535fa222d8904c79848fe90f1ffd40d5
FatlumG <167577293+FatlumG@users.noreply.github.com>
2024-11-26 22:15:56 +0100

    Added responsivity to navbar and added package-lock in gitignore

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,52 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { FaMoon } from "react-icons/fa";
-import { IoSunnyOutline } from "react-icons/io5";
+import React from "react";
 import Pfp from "../assets/images/pfp.svg";
+import { FiAlignRight } from "react-icons/fi";
+import UseToggle from "../hooks/UseToggle";
 import "../styles/components/navbar.css";
 
-function Toggle() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("preferredColorScheme");
-
-    const applyTheme = (theme) => {
-      setIsDarkMode(theme === "dark-mode");
-      document.body.dataset.theme = theme;
-    };
-
-    if (storedTheme) {
-      applyTheme(storedTheme);
-    } else {
-      const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-      applyTheme(prefersDarkScheme.matches ? "dark-mode" : "light-mode");
-
-      const themeChangeListener = (e) => {
-        applyTheme(e.matches ? "dark-mode" : "light-mode");
-      };
-
-      prefersDarkScheme.addEventListener("change", themeChangeListener);
-      return () => {
-        prefersDarkScheme.removeEventListener("change", themeChangeListener);
-      };
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    const newTheme = !isDarkMode ? "dark-mode" : "light-mode";
-    setIsDarkMode(!isDarkMode);
-    document.body.dataset.theme = newTheme;
-    localStorage.setItem("preferredColorScheme", newTheme);
-  };
-
+function Toggle({ toggleSidebar }) {
   return (
-    <div className="toggle position-relative d-flex align-items-center justify-content-between rounded-5">
-      {isDarkMode ? (
-        <IoSunnyOutline className="text-white tIcon" onClick={toggleTheme} />
-      ) : (
-        <FaMoon className="tIcon" onClick={toggleTheme} />
-      )}
+    <div className="toggle position-relative d-flex align-items-center justify-content-between gap-3 rounded-5">
+      <UseToggle className="d-none d-lg-block" />
       <img src={Pfp} alt="Profile Photo" className="pfp" />
+      <FiAlignRight
+        className="bars d-sm-block d-lg-none fs-3"
+        onClick={toggleSidebar}
+      />
     </div>
   );
 }
---
+++ src/hooks/UseToggle.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { FaMoon } from "react-icons/fa";
+import { IoSunnyOutline } from "react-icons/io5";
+import "../styles/components/navbar.css";
+
+function ToggleHook(props) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("preferredColorScheme");
+
+    const applyTheme = (theme) => {
+      setIsDarkMode(theme === "dark-mode");
+      document.body.dataset.theme = theme;
+    };
+
+    if (storedTheme) {
+      applyTheme(storedTheme);
+    } else {
+      const prefersDarkScheme = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      );
+      applyTheme(prefersDarkScheme.matches ? "dark-mode" : "light-mode");
+
+      const themeChangeListener = (e) => {
+        applyTheme(e.matches ? "dark-mode" : "light-mode");
+      };
+
+      prefersDarkScheme.addEventListener("change", themeChangeListener);
+      return () => {
+        prefersDarkScheme.removeEventListener("change", themeChangeListener);
+      };
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode ? "dark-mode" : "light-mode";
+    setIsDarkMode(!isDarkMode);
+    document.body.dataset.theme = newTheme;
+    localStorage.setItem("preferredColorScheme", newTheme);
+  };
+
+  return (
+    <div className={props.className}>
+      {isDarkMode ? (
+        <div style={props.styleS} onClick={toggleTheme}>
+          <IoSunnyOutline className="text-white tIcon mb-1" />{" "}
+          <span>{props.sun}</span>
+        </div>
+      ) : (
+        <div style={props.styleM} onClick={toggleTheme}>
+          <span>{props.moon}</span> <FaMoon className="tIcon mb-1" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ToggleHook;
