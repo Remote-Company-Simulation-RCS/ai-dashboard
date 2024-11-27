7a66a71301a2c2188bc082a0f3767c9c4bb40b67
FatlumG <167577293+FatlumG@users.noreply.github.com>
2024-11-27 22:02:37 +0100

    Added responsive to Subscription page

--- src/components/SubtableCard.jsx
+++ src/components/SubtableCard.jsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function SubCard(props) {
-   
   const [isActive, setIsActive] = useState(false);
 
   const handleClick = () => {
     setIsActive(!isActive);
   };
 
-    return (
-        <div className={`sub-card ${isActive ? 'sub-active' : ''} d-flex gap-3 justify-content-start align-items-center`} onClick={handleClick}   >
-            <div className='sub-card-image  d-flex justify-content-center align-items-center '>
-                {props.subIcon}
-            </div>
-            <div className='sub-card-text d-flex flex-column justify-content-center '>
-                <h6 className='tarif-title'>{props.title}</h6>
-                <h3 className='tarif-text fw-bold'>{props.text}</h3>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`sub-card ${
+        isActive ? "sub-active" : ""
+      } d-flex gap-3 justify-content-start align-items-center mb-4`}
+      onClick={handleClick}
+    >
+      <div className="sub-card-image d-flex justify-content-center align-items-center">
+        {props.subIcon}
+      </div>
+      <div className="sub-card-text d-flex flex-column justify-content-center ">
+        <h6 className="tarif-title">{props.title}</h6>
+        <h3 className="tarif-text fw-bold">{props.text}</h3>
+      </div>
+    </div>
+  );
 }
