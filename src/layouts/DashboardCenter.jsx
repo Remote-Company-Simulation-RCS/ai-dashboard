2ab3b35f1a81cfe8575fec3ec08825482a554cde
user.bunjakuezdrit@gmail.com <user.bunjakuezdrit@gmail.com>
2024-12-08 17:16:47 +0100

    added statistic dashboard

--- src/layouts/DashboardCenter.jsx
+++ src/layouts/DashboardCenter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BlueChart from "../components/BlueChart";
+import StatisticChart from "../components/Statistic.jsx";
 
 function DashboardCenter() {
     return (
-        <div className="dashboard-center">
+        <div className="dashboard-center d-flex justify-content-between flex-column">
             <BlueChart />
+            <StatisticChart />
         </div>
     );
 }
