d6920d68571466a08ef176e55ecc8f1c7b08ac3d
ezdrit <bunjakuezdrit@gmail.com>
2024-12-08 20:24:54 +0100

    readded statistic dashboard

--- src/layouts/DashboardCenter.jsx
+++ src/layouts/DashboardCenter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BlueChart from "../components/BlueChart";
+import Statistic from '../components/Statistic';
 
 function DashboardCenter() {
     return (
-        <div className="dashboard-center">
+        <div className="dashboard-center d-flex justify-content-between flex-column">
             <BlueChart />
+            <Statistic />
         </div>
     );
 }
